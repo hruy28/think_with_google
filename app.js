418cd33c3f4115921c6a8ab2aaaa31e9199f8f2e
hruy28 <hruyw@yahoo.com>
2017-01-14 11:49:04 +0100

    changes in the app.js

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
-var fs = require('fs');
-
 var app = express();
+var path = require('path');
 
-app.set('views',__dirname + '/views');
 app.use(express.static(__dirname+'/public'));
+app.set('views',__dirname + '/views');
 
-app.listen(8000);
-console.log('Server is now running ...');
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+app.get('/micromoments.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/micromoments.html'));
+});
+app.get('/consumer_trend.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/consumer_trend.html'));
+});
+app.get('/video.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/video.html'));
+});
+
+var port_number = 8000;
+app.listen(port_number);
+console.log('Server is now running on port ' + port_number);
